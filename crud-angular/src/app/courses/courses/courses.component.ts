e4e7e4394f3309a12957675abb0b03ae6379c0e6
Alexson Felipe <alexson.silveira@triersistemas.com.br>
2022-12-30 22:01:47 -0300

    Adicionando o projeto back-end com Spring

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,43 +1,40 @@
 import { CoursesService } from '../services/courses.service';
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
-import {Observable, of} from 'rxjs';
-import {MatDialog} from "@angular/material/dialog";
-import {ErrorDialogComponent} from "../../shared/components/error-dialog/error-dialog.component";
-import {error} from "@angular/compiler-cli/src/transformers/util";
+import { catchError, Observable, of } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from '../../shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
-
   courses$: Observable<Course[]>;
   // courses: Course[] = [];
-  displayedColumns = ['name','category'];
+  displayedColumns = ['name', 'category'];
 
   // coursesService: CoursesService;
 
   constructor(public dialog: MatDialog, private coursesService: CoursesService) {
     // this.courses = [];
     // this.coursesService = new CoursesService();
-    this.courses$ = this.coursesService.list().pipe(error => {
-      this.onError('Erro ao Carregar cursos.');
-      return of([])
-    });
+    this.courses$ = this.coursesService.list().pipe(
+      catchError((error) => {
+        this.onError('Erro ao Carregar cursos.');
+        return of([]);
+      })
+    );
 
     // this.coursesService.list().subscribe(courses => this.courses = courses);
   }
 
   onError(errorMsg: string) {
     this.dialog.open(ErrorDialogComponent, {
-      data: errorMsg
+      data: errorMsg,
     });
   }
 
-  ngOnInit(): void {
-
-  }
-
+  ngOnInit(): void {}
 }
--- crud-angular/src/app/shared/shared.module.ts
+++ crud-angular/src/app/shared/shared.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ErrorDialogComponent } from './components/error-dialog/error-dialog.component';
-import {AppMaterialModule} from "./app-material/app-material.module";
+import { AppMaterialModule } from './app-material/app-material.module';
+import { CategoryPipe } from './pipes/category.pipe';
 
 @NgModule({
-  declarations: [
-    ErrorDialogComponent
-  ],
-  imports: [
-    AppMaterialModule,
-    CommonModule
-  ],
-  exports: [
-    ErrorDialogComponent
-  ]
+  declarations: [ErrorDialogComponent, CategoryPipe],
+  imports: [AppMaterialModule, CommonModule],
+  exports: [ErrorDialogComponent, CategoryPipe],
 })
-export class SharedModule { }
+export class SharedModule {}
