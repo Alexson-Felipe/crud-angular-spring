fe3fef573d0d448ffcafe6fca0efe05a00f2565a
Alexson Felipe <alexson.silveira@triersistemas.com.br>
2023-01-08 18:29:06 -0300

    Ajuste

--- crud-angular/src/app/courses/courses.module.ts
+++ crud-angular/src/app/courses/courses.module.ts
@@ -6,9 +6,10 @@ import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
 import { SharedModule } from '../shared/shared.module';
 import { CourseFormComponent } from './course-form/course-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [CoursesComponent, CourseFormComponent],
-  imports: [CommonModule, CoursesRoutingModule, AppMaterialModule, SharedModule],
+  imports: [CommonModule, CoursesRoutingModule, AppMaterialModule, SharedModule, ReactiveFormsModule],
 })
 export class CoursesModule {}
